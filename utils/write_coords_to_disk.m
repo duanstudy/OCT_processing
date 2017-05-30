1a4c6a7a63543ae0ae37002c7cbc13b199593c65
petteriTeikari <petteri.teikari@gmail.com>
2017-05-30 15:13:30 +0100

    Add here the custom ROIs
    needs some more modification of code though

--- utils/write_coords_to_disk.m
+++ utils/write_coords_to_disk.m
@@ -6,6 +6,18 @@ function write_coords_to_disk(fileOut, output_cell)
     fprintf(fid, '%s\t %s\t %s\t %s\t %s\t %s\t %s\n', 'filename','z-min','z-max', ...
                                  'left_min','left_max','right_min','right_max');       
 
+    % TODO! Not very adaptive way of writing a lot of variables to disk
+    % now! You way want to make a loop here that allows you to specify also
+    % a lot more ROIs on top of the rough crop indices for example?
+
+    % For example if you want to calculate the ratios from multiple retinal
+    % locations, 
+    
+    % TODO! Now also the reading part (get_file_specs.m) is quickly written 
+    % and decoupled from this function, and you want to make the functions so that you would
+    % only need to change the fields from one place, and still both reading
+    % and writing would work
+                             
     for i = 1:size(output_cell,1)
 
         fprintf(fid,'%s\t %d\t %d\t %d\t %d\t %d\t %d\n', ...
